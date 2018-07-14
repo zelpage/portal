83205922c59391e6358f34eedc15d653c371d5bb
Josef Petrak <josef.petrak@somemove.cz>
2018-07-14 15:53:14 +0200

    PHP file formatting

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,12 +2,13 @@
 
 	namespace ZelPage;
 
+	use \Nette\Application\UI\ITemplate;
 	use \Nette\Application\UI\Presenter;
 	use \Nette\Utils\DateTime;
 
 	abstract class BasePresenter extends Presenter {
 
-		protected function createTemplate() {
+		protected function createTemplate(): ITemplate {
 			$template = parent::createTemplate();
 			$template->today = new DateTime();
 			return $template;
